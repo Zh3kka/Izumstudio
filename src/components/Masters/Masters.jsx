2a331c491d9eade0d8745de4f6e2c06c546dff39
Евгений Селезнев <pix.12@laptop.pix.123@yandex.ru>
2022-11-04 13:33:33 +0300

    Fix review

--- src/components/Masters/Masters.jsx
+++ src/components/Masters/Masters.jsx
@@ -8,8 +8,66 @@ import car4 from '../../img/car4.jpg'
 import car5 from '../../img/car5.jpg'
 
 const Masters = () => {
+	// const masters = [
+	// 	{
+	// 		imageSrc: { car1 },
+	// 		alt: 'master1',
+	// 		nameMaster: 'Суприканова Аксинья',
+	// 		location: 'м. павелецкая',
+	// 		description:
+	// 			'Закончила МГМСУ имени А. И. Евдокимова, имеется опыт работы в медицине и косметологии более 3 лет. Готова развиваться в своей сфере вместе Вами! Жду Вас у себя на процедуре!',
+	// 	},
+	// 	{
+	// 		imageSrc: { car2 },
+	// 		alt: 'master2',
+	// 		nameMaster: 'Суприканова Аксинья',
+	// 		location: 'м. павелецкая',
+	// 		description:
+	// 			'Закончила МГМСУ имени А. И. Евдокимова, имеется опыт работы в медицине и косметологии более 3 лет. Готова развиваться в своей сфере вместе Вами! Жду Вас у себя на процедуре!',
+	// 	},
+	// 	{
+	// 		imageSrc: { car3 },
+	// 		alt: 'master3',
+	// 		nameMaster: 'Суприканова Аксинья',
+	// 		location: 'м. павелецкая',
+	// 		description:
+	// 			'Закончила МГМСУ имени А. И. Евдокимова, имеется опыт работы в медицине и косметологии более 3 лет. Готова развиваться в своей сфере вместе Вами! Жду Вас у себя на процедуре!',
+	// 	},
+	// 	{
+	// 		imageSrc: { car4 },
+	// 		alt: 'master4',
+	// 		nameMaster: 'Суприканова Аксинья',
+	// 		location: 'м. павелецкая',
+	// 		description:
+	// 			'Закончила МГМСУ имени А. И. Евдокимова, имеется опыт работы в медицине и косметологии более 3 лет. Готова развиваться в своей сфере вместе Вами! Жду Вас у себя на процедуре!',
+	// 	},
+	// 	{
+	// 		imageSrc: { car5 },
+	// 		alt: 'master5',
+	// 		nameMaster: 'Суприканова Аксинья',
+	// 		location: 'м. павелецкая',
+	// 		description:
+	// 			'Закончила МГМСУ имени А. И. Евдокимова, имеется опыт работы в медицине и косметологии более 3 лет. Готова развиваться в своей сфере вместе Вами! Жду Вас у себя на процедуре!',
+	// 	},
+	// ]
 	return (
 		<div className={styles.cardGroup} id='Masters'>
+			{/* {this.masters.map((id, master) => (
+				<>
+					<div className={styles.card}>
+						<img
+							src={master.imageSrc}
+							alt={master.alt}
+							width={360}
+							height={360}
+						/>
+						<h2 className={styles.cardTitle}>{master.nameMaster}</h2>
+						<span>{master.location}</span>
+						<p>{master.description}</p>
+					</div>
+				</>
+			))} */}
+
 			<div className={styles.card}>
 				<img src={car1} alt='master1' width={360} height={360} />
 				<h2 className={styles.cardTitle}>Супрунова Аксинья</h2>
