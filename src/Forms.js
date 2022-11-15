49c2d7df6e1346f133937e35b1f309b015166369
Mark <pix.123@yandex.ru>
2022-11-15 18:08:45 +0300

    Create a widget for amocrm

---
+++ src/Forms.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const NewComponent = React.createClass({
+	render: function () {
+		return (
+			<div>
+				!(function (a, m, o, c, r, m) {'{'}
+				;(a[o + c] = a[o + c] || {'{'}
+				setMeta: function (p) {'{'}
+				this.params = (this.params || []).concat([p])
+				{'}'},{'}'}), (a[o + r] = a[o + r] || function (f) {'{'}
+				a[o + r].f = (a[o + r].f || []).concat([f])
+				{'}'}), a[o + r]({'{'}
+				id: '1029478', hash: '003c1aa1e1ba21881e72e66a186afca3', locale: 'ru',
+				{'}'}), (a[o + m] = a[o + m] || function (f, k) {'{'}
+				a[o + m].f = (a[o + m].f || []).concat([[f, k]])
+				{'}'}){'}'})(window, 0, 'amo_forms_', 'params', 'load', 'loaded')
+				id='amoforms_script_1029478' async='async' charset='utf-8'
+				src='https://forms.amocrm.ru/forms/assets/js/amoforms.js?1668521588'
+			</div>
+		)
+	},
+})
+
+export default NewComponent
